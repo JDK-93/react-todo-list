7f9f0360003b722853a12c64362ffef6b4ba466e
JDK-93 <jonathankrucheski@gmail.com>
2020-12-29 16:46:27 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-wrap content-center justify-center bg-gray-800 ">
+        <img src={logo} className="animate-spin h-60 w-60" alt="logo" />
+    </div>
+  );
+}
+
+
+export default App;
